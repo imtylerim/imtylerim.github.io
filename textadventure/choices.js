15a2cafb7f56e19a5944ff9a0ecddaa573662792
tyler.im <tji2105@columbia.edu>
2015-04-23 17:58:45 -0400

    debugging to figure out why replaceChild() isn’t working

--- textadventure/choices.js
+++ textadventure/choices.js
@@ -1,30 +1,37 @@
 /*	Room choices	*/
 
 function work() {
+
 	var parent = document.getElementById("body");
 	var textNode = document.getElementById("text");
-	var newTextNode = document.createTextNode("You work for fifteen minutes.")
+	var newTextNode = document.createTextNode("You work for twenty minutes.\n\n")
 	parent.appendChild(newTextNode);
 	parent.replaceChild(newTextNode, textNode);
-	increment(15);
+	increment(20);
+	console.log(textNode);
+	console.log(newTextNode);
 }
 
 function masturbate() {
+
 	var parent = document.getElementById("body");
 	var textNode = document.getElementById("text");
-	var newTextNode = document.createTextNode("You masturbate for fifteen minutes.")
+	var newTextNode = document.createTextNode("You masturbate for fifteen minutes.\n\n")
 	parent.appendChild(newTextNode);
 	parent.replaceChild(newTextNode, textNode);
 	increment(15);
+
 }
 
 function watchTV() {
+
 	var parent = document.getElementById("body");
 	var textNode = document.getElementById("text");
-	var newTextNode = document.createTextNode("You watch Adventure Time for fifteen minutes.")
+	var newTextNode = document.createTextNode("You watch Adventure Time for thirty minutes.\n\n")
 	parent.appendChild(newTextNode);
 	parent.replaceChild(newTextNode, textNode);
-	increment(15);
+	increment(30);
+
 }
 
 
