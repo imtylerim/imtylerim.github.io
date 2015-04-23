892358836938a282572dda98d02ea33ad01f7cce
tyler.im <tji2105@columbia.edu>
2015-04-23 18:11:50 -0400

    Alright so now it appends text, instead of replacing

--- textadventure/choices.js
+++ textadventure/choices.js
@@ -3,7 +3,7 @@
 function work() {
 
 	var para = document.createElement("p");
-	var node = document.createTextNode("You work for twenty minutes");
+	var node = document.createTextNode("You work for 20 minutes.");
 	para.appendChild(node);
 
 	var element = document.getElementById("text");
@@ -14,22 +14,26 @@ function work() {
 
 function masturbate() {
 
-	var parent = document.getElementById("body");
-	var textNode = document.getElementById("text");
-	var newTextNode = document.createTextNode("You masturbate for fifteen minutes.\n\n")
-	parent.appendChild(newTextNode);
-	parent.replaceChild(newTextNode, textNode);
+	var para = document.createElement("p");
+	var node = document.createTextNode("You masturbate for 15 minutes.");
+	para.appendChild(node);
+
+	var element = document.getElementById("text");
+	element.appendChild(para);
+
 	increment(15);
 
 }
 
 function watchTV() {
 
-	var parent = document.getElementById("body");
-	var textNode = document.getElementById("text");
-	var newTextNode = document.createTextNode("You watch Adventure Time for thirty minutes.\n\n")
-	parent.appendChild(newTextNode);
-	parent.replaceChild(newTextNode, textNode);
+	var para = document.createElement("p");
+	var node = document.createTextNode("You watch Adventure Time for 30 minutes");
+	para.appendChild(node);
+
+	var element = document.getElementById("text");
+	element.appendChild(para);
+
 	increment(30);
 
 }
