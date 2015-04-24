fc70a1f21adebf1eb7ce74cc841060c4194a44e9
tyler.im <tji2105@columbia.edu>
2015-04-24 00:30:32 -0400

    fixed choices.js a lot

--- textadventure/choices.js
+++ textadventure/choices.js
@@ -2,93 +2,101 @@
 
 function work() {
 
-	var para = document.createElement("p");
-	var node;
 	var times = Number(localStorage.getItem("timesWorked"));
-	console.log(times);
 
-	switch (times) {
+	if (times < 5) {
+		var para = document.createElement("p");
+		var node;
+		console.log(times);
 
-		case 0:
-		case 1:
-			node = document.createTextNode("You work for 20 minutes.");
-			break;
+		switch (times) {
+			case 0:
+			case 1:
+				node = document.createTextNode("You work for 20 minutes.");
+				break;
+			case 2:
+				node = document.createTextNode("You work for 20 minutes. The work is never ending.");
+				break;
+			case 3:
+				node = document.createTextNode("You work for 20 minutes. Your hand aches. 
+					Your head is swimming.");
+				break;
+			case 4:
+				node = document.createTextNode("You work for 20 minutes. You'll never finish. 
+					You decide to stop working for tonight.");
+				break;
+		}
 
-		case 2:
-			node = document.createTextNode("You work for 20 minutes. The work is never ending.");
-			break;
-
-		case 3:
-			node = document.createTextNode("You work for 20 minutes. Your hand aches. Your head is swimming.");
-			break;
-
-		case 4:
-			node = document.createTextNode("You work for 20 minutes. You'll never finish. You decide to stop working for tonight.");
-			break;
+		para.appendChild(node);
+		var element = document.getElementById("text");
+		element.appendChild(para);
+		localStorage.setItem("timesWorked", times + 1);
+		increment(20);
 	}
-
-	para.appendChild(node);
-	var element = document.getElementById("text");
-	element.appendChild(para);
-	localStorage.setItem("timesWorked", times + 1);
-	increment(20);
 }
 
 function masturbate() {
 
-	var para = document.createElement("p");
-	var node;
 	var times = Number(localStorage.getItem("timesMasturbated"));
-	console.log(times);
 
-	switch(times) {
+	if (times < 6) {
+		var para = document.createElement("p");
+		var node;
+		console.log(times);
 
-		case 0:
-		case 1:
-		case 2: 
-			node = document.createTextNode("You idly masturbate for 15 minutes.");
-			break;
+		switch(times) {
+			case 0:
+			case 1:
+			case 2: 
+				node = document.createTextNode("You idly masturbate for 15 minutes.");
+				break;
+			case 3:
+				node = document.createTextNode("You masturbate for 15 more minutes. 
+					Your genitals feel irritated.");
+				break;
+			case 4:
+				node = document.createTextNode("You masturbate for another 15 minutes. 
+					Your genitals are painful to touch. You should probably stop.");
+				break;
+			case 5:
+				node = document.createTextNode("You still masturbate again for 15 minutes. 
+					Your ravaged genitals cry out in pain. They have been rendered unusable.")
+				break;
+		}
 
-		case 3:
-			node = document.createTextNode("You masturbate for 15 more minutes. Your genitals feel irritated.");
-			break;
-
-		case 4:
-			node = document.createTextNode("You masturbate for another 15 minutes. Your genitals are painful to touch. You should probably stop.");
-			break;
-
-		case 5:
-			node = document.createTextNode("You still masturbate again for 15 minutes. Your ravaged genitals cry out in pain. They have been rendered unusable.")
-			break;
-
+		para.appendChild(node);
+		var element = document.getElementById("text");
+		element.appendChild(para);
+		localStorage.setItem("timesMasturbated", times + 1);
+		increment(15);
 	}
-
-	para.appendChild(node);
-		
-	var element = document.getElementById("text");
-	element.appendChild(para);
-	localStorage.setItem("timesMasturbated", times + 1);
-	increment(15);
-
 }
 
 function watchTV() {
-
-	var para = document.createElement("p");
-	var node;
+	
 	var times = Number(localStorage.getItem("timesWatched"));
+	if (times < 3) {
 
-	if (times < 2)
-		node = document.createTextNode("You watch Adventure Time for 30 minutes.");
-	else 
-		node = document.createTextNode("You watch Adventure Time for 30 minutes. There are no more new episodes.");
+		var para = document.createElement("p");
+		var node;
 
-	para.appendChild(node);
-	var element = document.getElementById("text");
-	element.appendChild(para);
-	localStorage.setItem("timesWatched", times + 1);
-	increment(30);
+		switch (time) {
+			case 0:
+			case 1:
+				node = document.createTextNode("You watch Adventure Time for 30 minutes.");
+				break;
+			case 2:
+				node = document.createTextNode("You watch Adventure Time for 30 minutes. 
+					There are no more new episodes.");
+				break;
+		}
 
+		para.appendChild(node);
+		var element = document.getElementById("text");
+		element.appendChild(para);
+		localStorage.setItem("timesWatched", times + 1);
+		increment(30);
+	}
 }
 
 
