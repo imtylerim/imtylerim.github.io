b753ecc9b2a830d57b8a0518eec7a6b878137711
tyler.im <tji2105@columbia.edu>
2015-04-24 00:45:19 -0400

    same thing

--- textadventure/choices.js
+++ textadventure/choices.js
@@ -6,28 +6,29 @@ function work() {
 
 	if (times < 5) {
 		var para = document.createElement("p");
-		var node;
-		console.log(times);
+		var string;
 
 		switch (times) {
 			case 0:
 			case 1:
-				node = document.createTextNode("You work for 20 minutes.");
+				string = "You work for 20 minutes.";
 				break;
 			case 2:
-				node = document.createTextNode("You work for 20 minutes. The work is never ending.");
+				string = "You work for 20 minutes. Your hand aches. The work is never ending.";
 				break;
 			case 3:
-				node = document.createTextNode("You work for 20 minutes. Your hand aches. 
-					Your head is swimming.");
+				string = "You work for 20 minutes. The room is spinning.";
+				string += "You begin to lose sense of what is and isn't real.";
 				break;
 			case 4:
-				node = document.createTextNode("You work for 20 minutes. You'll never finish. 
-					You decide to stop working for tonight.");
+				string = "You work for 20 minutes. You'll never finish."
+				string += "You decide to stop working for tonight.";
 				break;
 		}
 
+		var node = document.createTextNode(string);
 		para.appendChild(node);
+
 		var element = document.getElementById("text");
 		element.appendChild(para);
 		localStorage.setItem("timesWorked", times + 1);
@@ -41,30 +42,33 @@ function masturbate() {
 
 	if (times < 6) {
 		var para = document.createElement("p");
-		var node;
-		console.log(times);
+		var string;
 
 		switch(times) {
 			case 0:
 			case 1:
 			case 2: 
-				node = document.createTextNode("You idly masturbate for 15 minutes.");
+				string = "You idly masturbate for 15 minutes.";
 				break;
 			case 3:
-				node = document.createTextNode("You masturbate for 15 more minutes. 
-					Your genitals feel irritated.");
+				string = "You masturbate for 15 more minutes.";
+				string += "Your genitals feel irritated.";
 				break;
 			case 4:
-				node = document.createTextNode("You masturbate for another 15 minutes. 
-					Your genitals are painful to touch. You should probably stop.");
+				string = "You masturbate for another 15 minutes."
+				string += "Your genitals are painful to touch. You should probably stop.";
 				break;
 			case 5:
-				node = document.createTextNode("You still masturbate again for 15 minutes. 
-					Your ravaged genitals cry out in pain. They have been rendered unusable.")
+				string = "And yet, you choose to masturbate again.";
+				string += "Your ravaged genitals cry out in pain. They have been rendered unusable.";
+				string += "Hopefully you won't need them later";
+				localStorage.setItem("genitalsIntact", false);
 				break;
 		}
 
+		var node = document.createTextNode(string);
 		para.appendChild(node);
+
 		var element = document.getElementById("text");
 		element.appendChild(para);
 		localStorage.setItem("timesMasturbated", times + 1);
@@ -75,19 +79,25 @@ function masturbate() {
 function watchTV() {
 	
 	var times = Number(localStorage.getItem("timesWatched"));
-	if (times < 3) {
 
+	if (times < 3) {
 		var para = document.createElement("p");
-		var node;
+		var string;
 
 		switch (time) {
 			case 0:
+				string = "You watch Adventure Time for 30 minutes.";
+				string += "Finn and Jake are such bros. It is a pleasure to watch.";
+				break;
 			case 1:
-				node = document.createTextNode("You watch Adventure Time for 30 minutes.");
+				string = "You watch Louie for 30 minutes.";
+				string += "Hahaha! Poor Louie. He never learns.";
+				string += "You realize suddenly that you're a lazy piece of shit."
 				break;
 			case 2:
-				node = document.createTextNode("You watch Adventure Time for 30 minutes. 
-					There are no more new episodes.");
+				string = "You watch Steven Universe for 15 minutes.";
+				string += "Out of nowhere, a short circuit causes your TV to break. Oh no!"
+				string += "You cry for another 15 minutes. When will it all end, you wonder.";
 				break;
 		}
 
